ae34f8222541d5777b8f591c6c4628fd62f5745a
Akhilesh Rangani <akhileshrangani4@gmail.com>
2024-07-03 18:38:32 -0400

    waitlist creation

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,23 @@ export default function Home() {
       <Toaster />
 
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="md:h-full h-80 bg-[#FCD0A1] relative overflow-hidden">
-          <Image
-            src="/mobile-app.png"
-            alt="Mobile App Screenshots"
-            fill
-            className="object-contain mt-8 md:mt-24 px-14 object-bottom"
-          />
-        </div>
+      <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
+  <div className="md:h-full h-80 bg-[#111827] relative flex items-center justify-center overflow-hidden">
+    <Image
+      src="/mobile-app.png"
+      alt="App Screenshots"
+      fill
+      className="object-contain"
+    />
+    <h1 className="absolute text-white text-4xl md:text-6xl font-bold top-8 md:top-24 px-14">
+      findyourplace
+    </h1>
+  </div>
+</section>
 
         <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Join the conversation: Secure your spot on our Chat App!
+            Join the conversation: Secure your spot on our App!
           </h1>
           <p className="text-gray-500">
             Join the waitlist to be notified when our app is available!
