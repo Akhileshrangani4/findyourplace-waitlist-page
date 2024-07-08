afb36eed991756a83bd5dfc1270d72ab7bc55cb0
Akhilesh Rangani <akhileshrangani4@gmail.com>
2024-07-08 00:59:38 -0400

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -7,32 +7,19 @@ export default function Home() {
     <>
       <Toaster />
 
-      <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
-        <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="md:h-full h-80 bg-[#111827] relative flex items-center justify-center overflow-hidden">
-            <Image
-              src="/mobile-app.png"
-              alt="App Screenshots"
-              fill
-              className="object-contain"
-            />
-            <h1 className="absolute text-white text-4xl md:text-6xl font-bold top-8 md:top-24 px-14">
-              findyourplace
-            </h1>
-          </div>
-        </section>
+      
 
-        <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
-          <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Join the conversation: Secure your spot on our App!
-          </h1>
-          <p className="text-gray-500">
-            Join the waitlist to be notified when our app is available!
-          </p>
-
-          <EmailForm />
-        </main>
-      </section>
+      <div className="min-h-screen bg-[#1e1e1e] text-white flex flex-col justify-center items-center">
+      <main className="flex flex-col gap-8 mt-8 justify-center items-center px-6 pb-10 text-center">
+        <h1 className="font-semibold tracking-tight text-3xl leading-tight md:text-4xl max-w-lg">
+          findyourplace.
+        </h1>
+        <p className="text-gray-400">
+          Join the waitlist to be notified when our app is available!
+        </p>
+        <EmailForm />
+      </main>
+    </div>
     </>
   );
 }
